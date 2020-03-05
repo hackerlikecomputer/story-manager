5c4d57ae8daaa71aea0313505b2568395dd06a66
hackerlikecomputer <49451910+hackerlikecomputer@users.noreply.github.com>
2020-03-05 10:27:06 -0600

    docstring

--- story_manager.py
+++ story_manager.py
@@ -12,6 +12,8 @@ from win10toast import ToastNotifier
 
 
 class StoryManagerException(Exception):
+    """sends errors to windows 10 toast notification"""
+
     def __init__(self, msg):
         super().__init__(msg)
         self.n = ToastNotifier()
@@ -19,12 +21,16 @@ class StoryManagerException(Exception):
 
 
 class StoryManager:
+    """watches project directory for changes and updates stories spreadsheet"""
+
     def __init__(self):
         with open("settings.json", "r") as f:
             self.s = json.load(f)
         self.s["project_dir"] = Path(self.s["project_dir"])
 
     def load_data(self):
+        """loads the existing data, or creates an empty DataFrame"""
+
         f = self.s["project_dir"] / "stories.xlsx"
         if os.path.exists(f):
             status = "not loaded"
@@ -50,6 +56,17 @@ class StoryManager:
         return df
 
     def ignore_dir(self, dirname):
+        """
+        whether to ignore the directory.
+        will ignore all directories starting with "." or that are explicitly ignored
+        in settings.json
+
+        args:
+            dirname (str): name of directory to check
+
+        returns: bool
+        """
+
         if dirname.startswith(".") or any(
             dirname.lower() == i.lower() for i in self.s["ignore_subdirs"]
         ):
@@ -58,6 +75,14 @@ class StoryManager:
             return False
 
     def get_story_slug(self, dirname):
+        """get the story slug from the directory name
+
+        args:
+            dirname (str): name of directory to get name from
+
+        returns: str
+        """
+
         pat = re.compile(r"(?<=\d\d\d\d-\d\d-\d\d\s).*")
         m = pat.search(dirname)
         if m:
@@ -66,6 +91,15 @@ class StoryManager:
             raise StoryManagerException(f"Unable to get story slug from name {dirname}")
 
     def get_start_date(self, dirname):
+        """
+        get the start date of the story from the folder name
+
+        args:
+            dirname (str): name of directory to check
+
+        returns: str
+        """
+
         pat = re.compile(r"\d\d\d\d-\d\d-\d\d(?=\s\w+)")
         m = pat.search(dirname)
         if m:
@@ -74,11 +108,29 @@ class StoryManager:
             raise StoryManagerException(f"cannot get start date from name {dirname}")
 
     def get_mtime(self, dir):
+        """
+        recursively get latest modified time of any file in folder
+
+        args:
+            dir (str): full path to directory
+
+        returns: datetime object
+        """
+
         return datetime.fromtimestamp(
             max(os.path.getmtime(root) for root, _, _, in os.walk(dir))
         )
 
     def get_status(self, dir):
+        """
+        gets the status from a .status file
+
+        args:
+            dir (str): full path to directory
+
+        returns: str
+        """
+
         if isinstance(dir, str):
             dir = Path(dir)
         if not os.path.exists(dir / ".status"):
@@ -89,12 +141,32 @@ class StoryManager:
             return status
 
     def record_exists(self, slug, df):
+        """
+        check if the story is present in spreadsheet
+
+        args:
+            slug (str): story slug to check
+            df (DataFrame): data to draw from
+
+        returns: bool
+        """
+
         if slug in df["slug"].tolist():
             return True
         else:
             return False
 
     def get_index_by_slug(self, slug, df):
+        """
+        get the dataframe index given a story slug
+
+        args:
+            slug (str): slug to check
+            df (DataFrame): data to check in
+
+        returns: int
+        """
+
         s = df[df["slug"] == slug].index
         if len(s) > 1:
             raise StoryManagerException(f"found multple records for folder for {slug}")
@@ -105,6 +177,21 @@ class StoryManager:
         return index
 
     def update_existing(self, df, slug, category, start_date, mtime, status, path):
+        """
+        updates an existing record
+
+        args:
+            df (DataFrame): data to update
+            slug (str): story slug
+            category (str): story category
+            start_date (str): start date from folder name
+            mtime (datetime): last modified date
+            status (str): story status
+            path (str): path to story dir
+
+        returns: DataFrame
+        """
+
         index = self.get_index_by_slug(slug, df)
         row = {
             "slug": slug,
@@ -119,6 +206,21 @@ class StoryManager:
         return df
 
     def update_data(self, df, slug, category, start_date, mtime, status, path):
+        """
+        updates a record if it exists, or appends a new one
+
+        args:
+            df (DataFrame): data to update
+            slug (str): story slug
+            category (str): story category
+            start_date (str): start date from folder name
+            mtime (datetime): last modified date
+            status (str): story status
+            path (str): path to story dir
+
+        returns: DataFrame
+        """
+
         if self.record_exists(slug, df):
             df = self.update_existing(
                 df, slug, category, start_date, mtime, status, path
@@ -138,6 +240,15 @@ class StoryManager:
         return df
 
     def update_all(self, df):
+        """
+        updates all records
+
+        args:
+            df (DataFrame): data to update
+
+        returns: DataFrame
+        """
+
         for cat_dir in os.listdir(self.s["project_dir"]):
             if os.path.isdir(self.s["project_dir"] / cat_dir):
                 if not self.ignore_dir(cat_dir):
@@ -159,12 +270,31 @@ class StoryManager:
         return df
 
     def cleanup(self, df):
+        """
+        drops records for folders that no longer exist
+
+        args:
+            df (DataFrame): dataframe to clean up
+
+        returns: DataFrame
+        """
+
         df["exists"] = df.path.apply(lambda dir: os.path.exists(dir))
         df = df[df.exists]
         del df["exists"]
         return df
 
     def auto_fit_columns(self, df, sheet):
+        """
+        fits the columns to the size of their contents
+
+        args:
+            df (DataFrame): data to get sizes from
+            sheet (xlsxwriter sheet object): sheet to modify
+
+        returns: None
+        """
+
         for i, col in enumerate(df.columns):
             i = i + 1
             if col == "path":
@@ -175,10 +305,28 @@ class StoryManager:
                 sheet.set_column(i, i, max_len)
 
     def sort(self, df):
+        """
+        sort dataframe by mtime
+
+        args: 
+            df (DataFrame): dataframe to sort
+
+        returns: DataFrame
+        """
+
         df = df.sort_values("mtime", ascending=True).reset_index(drop=True)
         return df
 
     def save(self, df):
+        """
+        save and format excel file
+
+        args: 
+            df (DataFrame): dataframe to save
+
+        returns: None
+        """
+
         writer = pd.ExcelWriter(
             self.s["project_dir"] / "stories.xlsx", engine="xlsxwriter"
         )
